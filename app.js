5a2311f889191e517b69110b7c6328c2e770a4a9
Bumble <nick1092387456@gmail.com>
2021-07-30 03:40:44 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 //載入 express 並建構應用程式伺服器
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/todo-list', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}) //與資料庫連線，告知尋找資料庫
+const db = mongoose.connection //取得連線狀態
+
+//設定狀態回覆訊息
+//連線失敗
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+//連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 //設定網頁路由
 app.get('/', (req, res) => {
