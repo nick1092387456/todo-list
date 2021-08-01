83c9316fa5d42e76f5dc4a85c8cc33672b9a0f44
Bumble <nick1092387456@gmail.com>
2021-08-01 14:27:20 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const Todo = require('./models/todo')
 
 //設定樣板引擎
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -30,7 +31,10 @@ db.once('open', () => {
 
 //設定網頁路由
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() //取出 Todo model 裡的所有資料
+    .lean() //把 Mongoose 的 Model 物件轉換成乾淨的 Javascript 資料陣列
+    .then((todo) => res.render('index', { todo })) //將資料傳給index樣板
+    .catch((error) => console.log(error)) //錯誤處理
 })
 
 //設定port
