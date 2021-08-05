4de1fa67499a05ba7b1c4de91d6eec761fb633df
Bumble <nick1092387456@gmail.com>
2021-08-05 16:55:17 +0800

    add sort function

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ db.once('open', () => {
 app.get('/', (req, res) => {
   Todo.find() //取出 Todo model 裡的所有資料
     .lean() //把 Mongoose 的 Model 物件轉換成乾淨的 Javascript 資料陣列
+    .sort({ _id: 'asc' })
     .then((todos) => res.render('index', { todos })) //將資料傳給index樣板
     .catch((error) => console.log(error)) //錯誤處理
 })
@@ -73,7 +74,6 @@ app.get('/todos/:id/edit', (req, res) => {
 app.post('/todos/:id/edit', (req, res) => {
   const id = req.params.id
   const { name, isDone } = req.body
-  console.log(req.body)
   return Todo.findById(id)
     .then((todo) => {
       todo.name = name
