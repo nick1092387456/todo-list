60b127919ba6bc9934e4d10dba5c1db043f12ecd
Bumble <nick1092387456@gmail.com>
2021-08-18 17:34:52 +0800

    feat: modify todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,10 +1,39 @@
-const Todo = require('../todo') //載入todo model
+const bcrypt = require('bcryptjs') //載入密碼交密套件
+if (process.env.NODE_ENV !== 'production') {
+  //確認資料庫連線狀態是否需要使用環境變數套件
+  require('dotenv').config()
+}
+//載入必要資料庫資訊
+const Todo = require('../todo')
+const User = require('../user')
 const db = require('../../config/mongoose')
-
+//建立種子使用者
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678',
+}
+//密碼加密及將seedUser推入資料庫
 db.once('open', () => {
-  console.log('mongodb connected!')
-  for (let i = 0; i < 10; i++) {
-    Todo.create({ name: 'name-' + i })
-  }
-  console.log('done')
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER.password, salt))
+    .then((hash) =>
+
+      User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash,
+      })
+    )
+    .then((user) => {
+      const userId = user._id
+      for (let i = 0; i < 10; i++) {
+        Todo.create({ name: `name-${i}`, userId })
+      }
+    })
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
 })
